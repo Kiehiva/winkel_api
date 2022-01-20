db741a571b43705e1f698eb652281e29645068b3
Kiehiva Peterano <kiehiva.peterano@hotmail.com>
2022-01-20 16:31:00 +0100

    first config API Platform

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,21 +2,63 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
-use App\Repository\CategoryRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
-#[ApiResource()]
+#[ApiResource(
+    collectionOperations: [
+        'get' => [
+            'normalization_context' => ['groups' => 'read:categories']
+        ],
+        'post' => [
+            'denormalization_context' => ['groups' => 'create:categories']
+        ]
+    ],
+    itemOperations: [
+        'get' => [
+            'normalization_context' => ['groups' => 'read:categories']
+        ],
+        'patch' => [
+            'denormalization_context' => ['groups' => 'update:category']
+        ],
+        'delete'
+    ]
+)]
 class Category
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups(['read:categories'])]
     private $id;
 
     #[ORM\Column(type: 'string', length: 100)]
+    #[Groups(['read:categories', 'create:categories', 'update:category', 'read:subcategories'])]
+    #[
+        Assert\Length(
+            min: 5,
+            minMessage: "Le nom doit contenir au moins {{ limit }} caractères.",
+            max: 100,
+            maxMessage: "Le nom ne peut contenir plus de {{ limit }} caractères."
+        ),
+        Assert\NotBlank(message: "Le nom est obligatoire")
+    ]
     private $name;
+
+    #[ORM\OneToMany(mappedBy: 'category', targetEntity: Subcategory::class)]
+    #[Groups(['read:categories'])]
+    private $subcategories;
+
+    public function __construct()
+    {
+        $this->subcategories = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -31,6 +73,36 @@ class Category
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Subcategory[]
+     */
+    public function getSubcategories(): Collection
+    {
+        return $this->subcategories;
+    }
+
+    public function addSubcategory(Subcategory $subcategory): self
+    {
+        if (!$this->subcategories->contains($subcategory)) {
+            $this->subcategories[] = $subcategory;
+            $subcategory->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSubcategory(Subcategory $subcategory): self
+    {
+        if ($this->subcategories->removeElement($subcategory)) {
+            // set the owning side to null (unless already changed)
+            if ($subcategory->getCategory() === $this) {
+                $subcategory->setCategory(null);
+            }
+        }
 
         return $this;
     }
--- src/Entity/Subcategory.php
+++ src/Entity/Subcategory.php
@@ -2,21 +2,49 @@
 
 namespace App\Entity;
 
-use App\Repository\SubcategoryRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\SubcategoryRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: SubcategoryRepository::class)]
+#[ApiResource(
+    collectionOperations: [
+        'get' => ['normalization_context' => ['groups' => ['read:subcategories']]],
+        'post' => ['denormalization_context' => ['groups' => ['create:subcategories']]]
+    ],
+    itemOperations: [
+        'get' => ['normalization_context' => ['groups' => ['read:subcategories']]],
+        'patch' => ['denormalization_context' => ['groups' => ['update:subcategory']]],
+        'delete'
+    ]
+
+)]
 class Subcategory
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups(['read:subcategories'])]
     private $id;
 
     #[ORM\Column(type: 'string', length: 100)]
+    #[Groups(['create:subcategories', 'read:subcategories', 'update:subcategory', 'read:categories'])]
+    #[
+        Assert\Length(
+            min: 5,
+            minMessage: "Le nom doit contenir au moins {{ limit }} caractères",
+            max: 100,
+            maxMessage: "Le nom ne doit dépasser {{ limit }} caractères"
+        ),
+        Assert\NotBlank(message: "Le nom est obligatoire")
+    ]
     private $name;
 
     #[ORM\ManyToOne(targetEntity: Category::class, inversedBy: 'subcategories')]
+    #[Groups(['create:subcategories', 'read:subcategories', 'update:subcategory'])]
+    #[Assert\NotBlank(message: "Veuillez associer une catégorie")]
     private $category;
 
     public function getId(): ?int
